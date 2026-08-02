0f8ff5e6de4d3091c13b8565940222fd69f75834
Suchetamon27 <suchetamonbol@gmail.com>
2026-08-02 22:30:52 +0530

    Create __init__.py

---
+++ builtins/__init__.py
@@ -0,0 +1,10 @@
+from .base import builtin_registry, BuiltinCommand
+from . import filesystem
+from . import process
+from . import sysinfo
+from . import registry
+from . import env
+from . import history
+from . import alias
+
+__all__ = ["builtin_registry", "BuiltinCommand"]
